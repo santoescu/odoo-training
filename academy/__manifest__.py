54fda375885d0c77c378a1d757324cfb5cc1fd5f
santoescu <santoescu@gmail.com>
2024-01-12 19:27:31 +0000

    [ADD] nuevo modulo

---
+++ academy/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Odoo Academy',
+    'summary': """Modulo para el manejo de recurso y sesiones""",
+    'description':"""Modolo para el manejo de:
+        -recursos
+        -sesiones
+        -atenciones
+        """,
+    'license': 'OPL-1',
+    'author': 'santoescu',
+    'website': 'www.odoo.com',
+    'category': 'Çustom Modules/Tech Training',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'application': True,
+}
