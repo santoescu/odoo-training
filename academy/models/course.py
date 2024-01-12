4d0cdf38a7f4c4f2f045eafb851ade2d0e580dbf
Odoo Paas <paas-admin@odoo.com.test>
2024-01-12 22:43:44 +0000

    [add] fields

--- academy/models/course.py
+++ academy/models/course.py
@@ -1,5 +1,17 @@
-from odoo import models
+from odoo import fields, models
 
 class Couser(models.Model):
     _name = "academy.course"
     _description = "Couser Info"
+
+    name = fields.Char(string="Titulo", required=True)
+    active = fields.Boolean(string="Activo",default=True)
+    description = fields.Text()
+    level = fields.Selection(string="Nivel",
+                             selection=[
+                                 ('beginner','Beginner'),
+                                 ('intermediate','Intermediate'),
+                                 ('advanced','Advance'),
+                             ],
+                             copy=False)
+    
